ea4e5d894ab43a718a43683630d5c3a8da88e30b
stefanocasafranca <scasafrancal01@gmail.com>
2025-03-29 10:10:09 -0500

    Initial Commit

---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Stefano Casafranca on 3/29/25.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
