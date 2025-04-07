3bd0fbcf943d52d00314636a0389a1f56318f3ea
stefanocasafranca <scasafrancal01@gmail.com>
2025-04-06 22:25:09 -0500

    Added the width and height to infinity

---
+++ DiceRoller/DiceView.swift
@@ -0,0 +1,18 @@
+//
+//  DiceView.swift
+//  DiceRoller
+//
+//  Created by Stefano Casafranca on 3/29/25.
+//
+
+import SwiftUI
+
+struct DiceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DiceView()
+}
